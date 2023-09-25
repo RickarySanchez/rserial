6263cabe576a0f8991cfe3322d14426039cf6c44
Emmet Horgan <Emmet.Horgan@analog.com>
2023-09-25 17:41:31 +0100

    Update with working serial monitor !

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,52 @@
 use serialport;
+use std::io::{self, Write};
+use clap::{Arg, Command};
 
 fn main() {
-    println!("Hello, world!");
-    let ports = serialport::available_ports().expect("No ports found!");
-    for p in ports {
-        println!("{}", p.port_name);
+    let matches = Command::new("Welcome to rserial !")
+        .about("Rust based command line serial monitor tool")
+        .disable_version_flag(true)
+        .arg(
+            Arg::new("port")
+                .help("The device path to a serial port")
+                .use_value_delimiter(false)
+                .required(true),
+        )
+        .arg(
+            Arg::new("baud")
+                .help("The baud rate to connect at")
+                .use_value_delimiter(false)
+                .required(true)
+                .validator(valid_baud),
+        )
+        .get_matches();
+    
+        
+    let port_name = matches.value_of("port").unwrap();
+    let baud_rate = matches.value_of("baud").unwrap().parse::<u32>().unwrap();
+
+    let port = serialport::new("COM4", 115200).open();
+
+    match port {
+        Ok(mut port) => {
+            let mut buffer: Vec<u8> = vec![0; 1024];
+            loop {
+                match port.read(buffer.as_mut_slice()) {
+                    Ok(t) => io::stdout().write_all(&buffer[..t]).unwrap(),
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to open \"{}\". Error: {}", "COM4", e);
+            std::process::exit(1);
+        }
     }
+}
 
-    let com1 = serialport::new("COM1", 9600).open().expect("Failed to open port");
-
+fn valid_baud(val: &str) -> Result<(), String> {
+    val.parse::<u32>()
+        .map(|_| ())
+        .map_err(|_| format!("Invalid baud rate '{}' specified", val))
 }
